019ef82d36c743cdc6e6c7b8893577ff74198110
slimaidarismail <slimaidarismail@gmail.com>
2018-01-31 14:07:50 +0000

    =add hmida's picture into home page

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from globalpages.views import HomePage
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePage.as_view(), name="home-page")
 ]
+
+urlpatterns += staticfiles_urlpatterns()
